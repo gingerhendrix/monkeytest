6a4b8fe84982deab1b37e22aff89c13cabb4dea3
Gareth Andrew <gingerhendrix@hotmail.com>
2007-11-19 19:01:00 +0000

    Refactored TestRunner (should probably rename to TestManager or sthg). Removed unused UnitTestRunner
    git-svn-id: svn+ssh://gandrew.com/home/1439/users/.home/repos/GMTest/trunk@304 5d58421e-2428-0410-a994-821f96727b03

--- js/TestRunner.js
+++ js/TestRunner.js
@@ -8,37 +8,11 @@ var TestRunner = new function(){
   }
   
   this.run = function(){
-    this.runner.initRun(tests);
-    var self = this;
+    var runner = test.runner();
+    runner.initRun(tests);
     tests.forEach(function(test){
-      test.run(self);
+      test.run(runner);
     })
-    this.runner.finishRun();
+    runner.finishRun();
   }
-  
-  this.initRun = function(tests){
-    this.runner.initRun(tests);
-  }
-  
-  this.finishRun = function(){
-    this.runner.finishRun();
-  }
-  
-  this.testInit = function(test){
-    this.runner.testInit(test);
-  }
-  
-  this.testSuccess = function(test){
-    this.runner.testSuccess(test);
-  }
-  
-  this.testFailure = function(test, e){
-    this.runner.testFailure(test, e);
-  }
-  
-  this.testError = function(test, e){
-    this.runner.testError(test, e);
-  }
-
-  
 }();
